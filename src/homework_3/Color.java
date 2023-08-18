345861ed615b1707a4fdaa09af3a233d4279f297
JulAn-education <jul_an@mail.ru>
2023-08-18 10:48:57 +0300

    Home work 3

---
+++ src/homework_3/Color.java
@@ -0,0 +1,6 @@
+package homework_3;
+
+public enum Color {
+    RED,
+    BLACK
+}
---
+++ src/homework_3/Node.java
@@ -0,0 +1,13 @@
+package homework_3;
+
+class Node {
+    int value;
+    Color color;
+    Node left, right, parent;
+
+    public Node(int value) {
+        this.value = value;
+        color = Color.RED;
+        left = right = parent = null;
+    }
+}
